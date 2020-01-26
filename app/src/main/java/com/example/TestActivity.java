82ae21e2a32e9d44f2e2779ae1c34d1c0751f9fc
Ashutosh ojha <ashutosh.ojha@jungleworks.com>
2020-01-26 19:47:47 +0530

    on create

--- app/src/main/java/com/example/TestActivity.java
+++ app/src/main/java/com/example/TestActivity.java
@@ -1,4 +1,14 @@
 package com.example;
 
-public class TestActivity {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+public class TestActivity  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
